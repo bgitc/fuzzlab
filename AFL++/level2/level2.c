e5f9e9e37418663b06529d0d418856f202f0270d
bgitc <104237982+bgitc@users.noreply.github.com>
2025-04-11 11:42:48 +0900

    Update level2.c

--- AFL++/level2/level2.c
+++ AFL++/level2/level2.c
@@ -2,57 +2,98 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_USER_INPUT 256
-#define MAX_LOG_LENGTH 512
-#define MAX_LOG_ENTRIES 5
+#define MAX_ELEMENTS 10
 
 typedef struct {
-    char entries[MAX_LOG_ENTRIES][MAX_LOG_LENGTH];
-    int entryCount;
-} LogData;
+    int type;
+    union {
+        int int_value;
+        char *str_value;
+        float float_value;
+    } data;
+} Element;
 
-void initializeLogData(LogData *logData) {
-    logData->entryCount = 0;
-    for (int i = 0; i < MAX_LOG_ENTRIES; i++) {
-        memset(logData->entries[i], 0, MAX_LOG_LENGTH);
-    }
-}
+int parse_elements(FILE *file, Element *elements, int max_count) {
+    int count = 0;
+    unsigned char type;
 
-void loadLogEntriesFromFile(LogData *logData, const char *fileName) {
-    FILE *file = fopen(fileName, "r");
-    if (!file) {
-        printf("Failed to open file: %s\n", fileName);
-        return;
-    }
-    char buffer[MAX_LOG_LENGTH];
-    while (fgets(buffer, MAX_LOG_LENGTH, file) && logData->entryCount < MAX_LOG_ENTRIES) {
-        buffer[strcspn(buffer, "\n")] = 0;
-        strncpy(logData->entries[logData->entryCount], buffer, 2 * MAX_LOG_LENGTH);
-        logData->entries[logData->entryCount][MAX_LOG_LENGTH - 1] = '\0';
-        logData->entryCount++;
-    }
-    fclose(file);
-}
+    while (count < max_count && fread(&type, 1, 1, file) == 1) {
+        elements[count].type = type & 0x0F;
 
-void printLogData(const LogData *logData) {
-    printf("Log Entries:\n");
-    for (int i = 0; i < logData->entryCount; i++) {
-        printf("%d: %s\n", i + 1, logData->entries[i]);
+        switch (elements[count].type) {
+            case 1:
+                fread(&elements[count].data.int_value, sizeof(int), 1, file);
+                break;
+
+            case 2: {
+                unsigned char len;
+                fread(&len, 1, 1, file);
+                elements[count].data.str_value = (char *)malloc(len + 1);
+                if (elements[count].data.str_value && len > 0) {
+                    fread(elements[count].data.str_value, 1, len, file);
+                    elements[count].data.str_value[len] = '\0';
+                } else {
+                    if (elements[count].data.str_value) elements[count].data.str_value[0] = '\0';
+                }
+                break;
+            }
+
+            case 3:
+                fread(&elements[count].data.float_value, sizeof(float), 1, file);
+                break;
+
+            default:
+                if ((type >> 4) > 0) {
+                    int special_len = (type >> 4) * 10;
+                    char *special_buffer = (char *)malloc(special_len);
+                    elements[count].data.str_value = (char *)malloc(20); // fixed size buffer
+                    if (special_buffer && elements[count].data.str_value) {
+                        fread(special_buffer, 1, special_len, file);
+                        memcpy(elements[count].data.str_value, special_buffer,
+                               special_len);  // overflows fixed 20-byte heap buffer
+                        free(special_buffer);
+                    }
+                }
+                break;
+        }
+
+        count++;
     }
+
+    return count;
 }
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        printf("Usage: %s <log_file>\n", argv[0]);
+        printf("Usage: %s <input file>\n", argv[0]);
         return 1;
     }
 
-    LogData logData;
-    initializeLogData(&logData);
+    FILE *input = fopen(argv[1], "rb");
+    if (!input) {
+        printf("Failed to open file.\n");
+        return 1;
+    }
 
-    printf("Simple Logging System\n");
-    loadLogEntriesFromFile(&logData, argv[1]);
-    printLogData(&logData);
+    Element elements[MAX_ELEMENTS];
+    int count = parse_elements(input, elements, MAX_ELEMENTS);
 
+    for (int i = 0; i < count; i++) {
+        switch (elements[i].type) {
+            case 1:
+                printf("INT: %d\n", elements[i].data.int_value);
+                break;
+            case 2:
+            default:
+                printf("STR: %s\n", elements[i].data.str_value ? elements[i].data.str_value : "(null)");
+                if (elements[i].data.str_value) free(elements[i].data.str_value);
+                break;
+            case 3:
+                printf("FLOAT: %f\n", elements[i].data.float_value);
+                break;
+        }
+    }
+
+    fclose(input);
     return 0;
 }
